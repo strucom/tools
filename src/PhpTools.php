9739db651de19a6e3b2c8e58df0c31c0d8facfef
af <af@strucom.ae>
2025-07-03 13:38:19 +0400

    setNestedValue created

--- src/PhpTools.php
+++ src/PhpTools.php
@@ -40,4 +40,38 @@ class PhpTools
         );
     }
 
+    /**
+     * Sets a value in a nested array using the provided keys.
+     *
+     * This function dynamically creates or navigates through a nested array structure
+     * based on the given keys and assigns the specified value to the deepest level.
+     *
+     * @param array &$valueArray The array to modify. Passed by reference.
+     * @param array $keys An array of keys representing the path to the value.
+     * @param mixed $value The value to set at the specified path.
+     *
+     * @since 7.4.0
+     * @author af
+     */
+    public static function setNestedValue(array &$valueArray, array $keys, mixed $value): void
+    {
+        $current = &$valueArray; // Reference to the root of the array
+
+        // Iterate through all keys except the last one
+        foreach (array_slice($keys, 0, -1) as $key) {
+            // Create the nested array if it doesn't exist
+            if (!isset($current[$key]) || !is_array($current[$key])) {
+                $current[$key] = [];
+            }
+
+            // Move the reference deeper into the array
+            $current = &$current[$key];
+        }
+
+        // Use the last key to set the value
+        $lastKey = end($keys);
+        $current[$lastKey] = $value;
+    }
+
+
 }
