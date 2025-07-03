aa48f8bcab4c2c3ae8c5f786cabf288a78df2a4a
strucom <140485265+strucom@users.noreply.github.com>
2025-07-03 09:55:52 +0400

    Create DefaultTest.php

---
+++ tests/DefaultTest.php
@@ -0,0 +1,14 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class DefaultTest extends TestCase
+{
+    /**
+     * A basic test to ensure PHPUnit is working correctly.
+     */
+    public function testPhpUnitSetup(): void
+    {
+        $this->assertTrue(true, 'PHPUnit is set up correctly.');
+    }
+}
